6b04a4812fb576168a494da745e784ea5f3696ea
yagizemreozbilge <yemre.ozbilge@gmail.com>
2024-03-10 16:05:15 +0300

    Add delete functions

--- src/rental_management_lib/src/rental_management_lib.cpp
+++ src/rental_management_lib/src/rental_management_lib.cpp
@@ -1053,7 +1053,15 @@ int edit_rent_record(){
  * @return 0.
  */
 int delete_rent_record(){
-    return 0;
+    printf("\nPlease enter record number to delete:");
+    int RecordNumberToDelete;
+    scanf("%d", &RecordNumberToDelete);
+
+    if (file_line_delete("rent_records.bin", RecordNumberToDelete) == 0) {
+        return 0;
+    } else {
+        return -1;
+    }
 };
 
 /**
